f22ae09f0a450500da7810d05cce3f7716990002
joaorocha <joaoeduardorrocha@gmail.com>
2023-11-22 22:55:17 -0300

    fix links social medias and contact

--- components/footer/index.js
+++ components/footer/index.js
@@ -11,7 +11,7 @@ export class Footer extends HTMLElement {
                 <a href="https://github.com/JoaoEduardoRRocha" target="_blank"><img src="/img/github.png" alt=""></a>
                 <p>Github</p>
             
-                <a href="https://www.linkedin.com/in/joaoeduardorrocha/" target="_blank"><img src="/img/linkeedin.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/joaoedrocha/" target="_blank"><img src="/img/linkeedin.png" alt=""></a>
                 <p>Linkedin</p>
 
                 <a href="//wa.me/5521976747042" target="_blank"><img src="/img/whatsapp.png" alt=""></a>
@@ -30,7 +30,7 @@ export class Footer extends HTMLElement {
 
         <div class="container-social-medias">
             <strong class="curriculum">Currículo</strong>
-            <a href="https://drive.google.com/file/d/15XWXCZpUcF5fqh6q7bXJsYHzrqQJa1wI/view" target="_blank"><img src="/img/curriculoicon.png" alt=""></a>
+            <a href="https://drive.google.com/file/d/1vUZBDba9TtnSUgfuw1A_I2RYGNbayeJV/view?usp=drive_link" target="_blank"><img src="/img/curriculoicon.png" alt=""></a>
         </div>
     </footer>
       `;
